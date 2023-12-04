b95619c944ce864a03d38a6dc6a847151087dcd5
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-04 15:33:31 +0000

    project frame created for mvp pages

---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <section className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>This is the Contact Page</p>
+      </section>
+    )
+  }
---
+++ app/events/page.js
@@ -0,0 +1,7 @@
+export default function Events() {
+    return (
+      <section className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>This is the Events Page</p>
+      </section>
+    )
+  }
