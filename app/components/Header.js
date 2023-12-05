7c46c2fd17ad19b544736155a92ecac8bfba9517
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-05 10:35:17 +0000

    header spaced

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,10 @@
 export default function Header() {
     return (
-      <section className="flex width-screen h-4 flex-col items-center justify-between p-8 bg-white">
-        <p>This is the header</p>
+      <section className="grid grid-cols-3 gap-4 place-content-evenly place-items-center width-screen h-4 justify-between p-8 bg-navy">
+      <div className="flex"><p>Menu</p><p>About Us</p><p>Volunteer</p></div>
+      <div className="flex"><p>Logo</p></div>
+      <div className="flex"><p>EB</p><p>MU</p><p>LI</p><p>DC</p><p>X</p><p>IG</p></div>
+        
       </section>
     )
   }
