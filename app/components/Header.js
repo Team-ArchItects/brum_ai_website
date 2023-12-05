bfa0a32c12abac53219314063d8c25fa919e7779
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-05 10:52:00 +0000

    header items justified to left right and center

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
-      <section className="grid grid-cols-3 gap-4 place-content-evenly place-items-center width-screen h-4 justify-between p-8 bg-navy">
-      <div className="flex"><p>Menu</p><p>About Us</p><p>Volunteer</p></div>
-      <div className="flex"><p>Logo</p></div>
-      <div className="flex"><p>EB</p><p>MU</p><p>LI</p><p>DC</p><p>X</p><p>IG</p></div>
+      <section className="grid grid-cols-3 gap-4 place-content-evenly items-center width-screen h-4 justify-between p-8 bg-navy">
+      <div className="flex justify-start"><p>Menu</p><p>About Us</p><p>Volunteer</p></div>
+      <div className="flex justify-center"><p>Logo</p></div>
+      <div className="flex justify-end"><p>EB</p><p>MU</p><p>LI</p><p>DC</p><p>X</p><p>IG</p></div>
         
       </section>
     )
