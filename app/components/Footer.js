726ed241b7557890bd101a629fa2579d29b786ab
David Mason <david_mason_@hotmail.co.uk>
2023-12-05 16:19:02 +0000

    Footer logos added, needs correcting

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,33 +1,36 @@
 // import socLogo from "../../images/original.png"
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Footer() {
-    return (
-      <section className="flex flex-col gap-4 w-screen h justify-between p-4 bg-white text-black">
+  return (
+    <section className="flex flex-col w-screen justify-between">
+      <source srcSet="/assets/footer_black.png" media="(prefers-color-cheme: dark)" />
+      {/* <Image src="/assets/footer_white.png" alt="Footer graffiti" width="500" height={100} /> */}
+      <div className="bg-[url('/assets/footer_white.png')] w-screen h-48"></div>
         <div className="flex flex-wrap justify-center space-x-2 items-center text-center">
-        <Image  
-        src="/assets/original.png"
-        width={100}
-        height={100}
-        alt="School of Code logo"
-      />
-        <Image  
-        src="/assets/original.png"
-        width={100}
-        height={100}
-        alt="School of Code logo"
-      />
-      <p className='w-full p-3'>Supported by these Amazing Companies</p>
+          <Image
+            src="/assets/original.png"
+            width={100}
+            height={100}
+            alt="School of Code logo"
+          />
+          <Image
+            src="/assets/original.png"
+            width={100}
+            height={100}
+            alt="School of Code logo"
+          />
+          <p className="w-full p-3">Supported by these Amazing Companies</p>
         </div>
-        <div className='text-right'>
-        <p>Contact Us Button</p>
+        <div className="text-right">
+          <p>Contact Us Button</p>
         </div>
-        <div className='flex flex-wrap flex-col items-center'>
-        <div className='p-px bg-black w-10/12 h-0 '></div>
-        <div className='w-10/12'>
-        <p>&copy; Copyright 2023 BrumAI. All rights reserved.</p>
+        <div className="flex flex-wrap flex-col items-center">
+          <div className="p-px bg-black w-10/12 h-0 "></div>
+          <div className="w-10/12">
+            <p>&copy; Copyright 2023 BrumAI. All rights reserved.</p>
+          </div>
         </div>
-        </div>
-      </section>
-    )
-  }
+    </section>
+  );
+}
