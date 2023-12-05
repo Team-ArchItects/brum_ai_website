4405dd5e73034d87f20f47b8a02658eb28f50c7e
Tay Khan <taykhantexas@gmail.com>
2023-12-05 18:01:30 +0200

    Justify Footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Footer() {
     return (
       <section className="flex flex-col gap-4 w-screen h justify-between p-4 bg-white text-black">
-        <div className="flex justify-center space-x-2 items-center">
+        <div className="flex flex-wrap justify-center space-x-2 items-center text-center">
         <Image  
         src="/assets/original.png"
         width={100}
@@ -17,14 +17,15 @@ export default function Footer() {
         height={100}
         alt="School of Code logo"
       />
+      <p className='w-full p-3'>Supported by these Amazing Companies</p>
         </div>
-        <div className='flex flex-end'>
-        <p>This is the contact button</p>
+        <div className='text-right'>
+        <p>Contact Us Button</p>
         </div>
         <div className='flex flex-wrap flex-col items-center'>
         <div className='p-px bg-black w-10/12 h-0 '></div>
         <div className='w-10/12'>
-        <p>This is the copyright notice</p>
+        <p>&copy; Copyright 2023 BrumAI. All rights reserved.</p>
         </div>
         </div>
       </section>
