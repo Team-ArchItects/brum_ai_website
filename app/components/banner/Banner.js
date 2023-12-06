46cf34c7a2d8ff89da26e31585b31e145aa3add4
cal woodford <callum@woodford.info>
2023-12-06 14:53:29 +0000

    modified: grid

--- app/components/banner/Banner.js
+++ app/components/banner/Banner.js
@@ -15,10 +15,16 @@ import Button from 'app/components/banner/Button.js'
 const Banner = () => {
     return (
       <div className="bg-[url('/assets/cityphoto.png')] bg-cover bg-center w-screen h-96 flex-row">
+        <div className="grid grid-cols-1">
       <section className="flex justify-end mr-6 mt-6">
+      <div className="relative justify-end">
       <Button text={"Next event"} location={"/events"}/>
+      </div>
       </section>
+      <div className="relative w-64 md:w-80 lg:w-96 xl:w-102">
       <Image src="/assets/banner_content.png" width="500" height="250" alt="Banner" className="ml-12"/>
+      </div>
+      </div>
       </div>
     );
   }
