83a5873269d66997585ccf4429c507a6c374e53e
KatBaginska <katarzyna.baginska11@icloud.com>
2023-12-06 12:22:48 +0000

    text

--- app/components/events/Events.js
+++ app/components/events/Events.js
@@ -20,10 +20,10 @@ const Events = () => {
         <section className="text-citrus text-center text-3xl py-2">
             <h1>Upcoming Events:</h1>
                 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly items-center ">
-                    <div className="relative"><div className="bg-[url('/assets/event1_white.png')] bg-contain bg-center bg-no-repeat sm:w-64 md:w-80 lg:w-96 xl:w-102 sm:h-64 md:h-80 lg:h-96 xl:h-102 flex" >
+                    <div className="relative"><div className="bg-[url('/assets/event1_white.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex" >
                         <p className="py-2.5 pl-20 bottom-0 inset-x text-5xl  leading-4 absolute">OCT 24</p>
                     </div></div>
-                   <div className="relative"><div className="bg-[url('/assets/event2_white.png')] bg-contain bg-center bg-no-repeat sm:w-64 md:w-80 lg:w-96 xl:w-102 sm:h-64 md:h-80 lg:h-96 xl:h-102 flex">
+                   <div className="relative"><div className="bg-[url('/assets/event2_white.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex">
                         <p className="py-20 text-center bottom-0 inset-x text-5xl leading-4 absolute">NOV 24</p>
                     </div></div>
                 </section>
--- app/components/instagram_carousel/Instagram.js
+++ app/components/instagram_carousel/Instagram.js
@@ -20,10 +20,10 @@ const Events = () => {
         <section className="text-citrus text-center text-3xl py-2">
             <h1>Image Reel:</h1>
                 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly items-center text-citrus">
-                    <div className="bg-[url('/assets/ben.png')] bg-contain bg-center bg-no-repeat sm:w-64 md:w-80 lg:w-96 xl:w-102 sm:h-64 md:h-80 lg:h-96 xl:h-102 flex">
+                    <div className="bg-[url('/assets/ben.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex">
                         <p className="py-2.5 pl-20 bottom-0 inset-x text-5xl text-center leading-4">21/01/2024</p>
                     </div>
-                    <div className="bg-[url('/assets/sam.png')] bg-contain bg-center bg-no-repeat sm:w-64 md:w-80 lg:w-96 xl:w-102 sm:h-64 md:h-80 lg:h-96 xl:h-102 flex">
+                    <div className="bg-[url('/assets/sam.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex">
                         <p className="py-2.5 pl-20 bottom-0 inset-x text-5xl text-center leading-4">21/03/2024</p>
                     </div>
                 </section>
