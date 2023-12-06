08882bfed84549e96c186f2a51c6c6bd422046f7
KatBaginska <katarzyna.baginska11@icloud.com>
2023-12-06 15:03:53 +0000

    grid

--- app/components/banner/Banner.js
+++ app/components/banner/Banner.js
@@ -4,11 +4,11 @@ import Button from 'app/components/banner/Button.js'
 
 const Banner = () => {
     return (
-      <div className="bg-[url('/assets/cityphoto.png')] bg-cover bg-center w-screen h-96 grid grid-cols-6 grid-rows-3">
-      <section className="flex justify-end mr-6 mt-6 col-start-5">
+      <div className="bg-[url('/assets/cityphoto.png')] bg-cover bg-center w-screen h-96 grid grid-cols-12 grid-rows-6">
+      <section className="flex col-start-11 row-start-2">
       <Button text={"Next event"} location={"/events"}/>
       </section>
-      <Image src="/assets/banner_content.png" width="500" height="250" alt="Banner" className="ml-12 col-start-2 row-start-2"/>
+      <Image src="/assets/banner_content.png" width="500" height="250" alt="Banner" className="col-start-5 row-start-2 min-w-[450px] justify-self-end"/>
       </div>
     );
   }
