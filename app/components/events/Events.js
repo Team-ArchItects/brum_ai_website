434bdb34ea6cdb4027bd790678029987b3ce4a09
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-06 12:49:44 +0000

    text fomatting applied to events and insta comps

--- app/components/events/Events.js
+++ app/components/events/Events.js
@@ -1,5 +1,3 @@
-import imageFrame from '../../../public/assets/event1_white.png'
-
 const dummyData = [
     {
         "name": "University Event",
@@ -19,13 +17,17 @@ const Events = () => {
     return (
         <section className="text-citrus text-center text-3xl py-2">
             <h1>Upcoming Events:</h1>
-                <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly items-center ">
-                    <div className="relative"><div className="bg-[url('/assets/event1_white.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex" >
-                        <p className="py-2.5 pl-20 bottom-0 inset-x text-5xl  leading-4 absolute">OCT 24</p>
-                    </div></div>
-                   <div className="relative"><div className="bg-[url('/assets/event2_white.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex">
-                        <p className="py-20 text-center bottom-0 inset-x text-5xl leading-4 absolute">NOV 24</p>
-                    </div></div>
+                <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly">
+                        <div className="relative">
+                            <div className="bg-[url('/assets/event1_white.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex" >
+                                <p className="py-20 bottom-0 inset-x text-5xl leading-4 absolute m-auto left-0 right-0">OCT 24</p>
+                            </div>
+                        </div>
+                        <div className="relative">
+                            <div className="bg-[url('/assets/event2_white.png')] bg-contain bg-center bg-no-repeat w-64 md:w-80 lg:w-96 xl:w-102 h-64 md:h-80 lg:h-96 xl:h-102 flex">
+                                <p className="py-20 bottom-0 inset-x text-5xl leading-4 absolute m-auto left-0 right-0">NOV 24</p>
+                            </div>
+                        </div>
                 </section>
         </section>
     );
