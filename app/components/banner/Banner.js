72dcb100776b737cb98fb520dbb9a15a491d4b52
Tay Khan <taykhantexas@gmail.com>
2023-12-06 16:47:44 +0200

    Grid Added

--- app/components/banner/Banner.js
+++ app/components/banner/Banner.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import "./banner.css";
-// import banner from './assets/cityphoto';
 import Image from 'next/image';
 import Button from 'app/components/banner/Button.js'
 
-{/* <div className="banner">
-  <img src={banner} alt="Banner" />
-  <div className="banner-content">
-      <h1>aGuideHub</h1>
-      <p>Welcome To aGuideHub! </p>
-  </div>
-</div> */}
-
 const Banner = () => {
     return (
-      <div className="bg-[url('/assets/cityphoto.png')] bg-cover bg-center w-screen h-96 flex-row">
-      <section className="flex justify-end mr-6 mt-6">
+      <div className="bg-[url('/assets/cityphoto.png')] bg-cover bg-center w-screen h-96 grid grid-cols-6 grid-rows-3">
+      <section className="flex justify-end mr-6 mt-6 col-start-5">
       <Button text={"Next event"} location={"/events"}/>
       </section>
-      <Image src="/assets/banner_content.png" width="500" height="250" alt="Banner" className="ml-12"/>
+      <Image src="/assets/banner_content.png" width="500" height="250" alt="Banner" className="ml-12 col-start-2 row-start-2"/>
       </div>
     );
   }
