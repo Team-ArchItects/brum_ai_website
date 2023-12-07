9f7ab75267bbd5e61fd7900a5d1e006bf03ec3e3
David Mason <david_mason_@hotmail.co.uk>
2023-12-07 16:19:28 +0000

    Main pulled and tag names corrected

--- app/components/header components/Dropdown.js
+++ app/components/header components/Dropdown.js
@@ -11,27 +11,27 @@ function handleOnClick() {
 }
 return (
     <>
-<button onClick={()=>handleOnClick()} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-navy hover:bg-navy font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-navy" type="button">Menu <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+<button onClick={()=>handleOnClick()} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-navy hover:bg-navy font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-navy" type="button">Menu <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
 </svg>
 </button>
 
-<div id="dropdown" class={`z-10 ${dropdown} relative left-0 top-28 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
-    {/* <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+<div id="dropdown" className={`z-10 ${dropdown} relative left-0 top-28 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+    {/* <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
       <li>
-        <Link href="/" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Home</Link>
+        <Link href="/" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Home</Link>
       </li>
       <li>
-        <Link href="/events" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Events</Link>
+        <Link href="/events" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Events</Link>
       </li>
       <li>
-        <Link href="/contact" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Contact Us</Link>
+        <Link href="/contact" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Contact Us</Link>
       </li>
       <li>
-        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
       </li>
     </ul> */}
-    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
     {pages.map((menuItems) => {
           return <MenuItems key={menuItems.id} data={menuItems} />
           
