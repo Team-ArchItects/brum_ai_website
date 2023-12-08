1dad8fcfb70be7454d6ada9690b2ceb066cb4ede
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-08 15:11:40 +0000

    api called moved to separate component

--- app/events/components/EventList.js
+++ app/events/components/EventList.js
@@ -1,21 +1,21 @@
 'use client'
 
-import supabase from '../utils/supabase'
+import supabase from '../../utils/supabase'
 import {useEffect, useState } from 'react';
-import { notFound } from 'next/navigation'
+// import { notFound } from 'next/navigation'
 // import EventList from './components/EventList';
 
 const idExample = "ff05cd27-ed7a-4861-af3c-34a2c829d12c"
 
 // const supabase = createClient(URL, API);
 
-export default function Events() {
+export default function EventList() {
 
   const [testEvents, setTestEvents] = useState(null);
 
   useEffect(() => {
     const fetchEvents = async () => {
-      const { data, error } = await supabase.from('testEvents').select('*').eq('id', idExample)
+      const { data, error } = await supabase.from('testEvents').select('*')/*.eq('id', idExample)*/
 
       if (error) {
         console.error('Error', error.message);
--- app/events/page.js
+++ app/events/page.js
@@ -1,38 +1,11 @@
-'use client'
-
-import supabase from '../utils/supabase'
-import {useEffect, useState } from 'react';
-import { notFound } from 'next/navigation'
-// import EventList from './components/EventList';
-
-const idExample = "ff05cd27-ed7a-4861-af3c-34a2c829d12c"
-
-// const supabase = createClient(URL, API);
+import EventList from './components/EventList'
 
 export default function Events() {
-
-  const [testEvents, setTestEvents] = useState(null);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const { data, error } = await supabase.from('testEvents').select('*').eq('id', idExample)
-
-      if (error) {
-        console.error('Error', error.message);
-        return;
-      }
-
-      setTestEvents(data);
-  }
-  fetchEvents();
-  console.log(testEvents)
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-between px-24">
+    <h1 className=''>Events Page</h1>
+      <EventList />
+    </section>
+    
+  )
 }
-)}
-
-//   const { data: testEvents } = await supabase.from('testEvents').select('*').eq('id', idExample)
-// if (!testEvents) {
-//     return <p>No testEvents found.</p>
-//   }
-// {console.log(testEvents)}
-//   return <pre>{JSON.stringify(testEvents, null, 2)}</pre>
-// }
