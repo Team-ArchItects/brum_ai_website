8910d081398dd2af38f4c1015073ebf754b5d95f
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-08 14:37:24 +0000

    console logging db

---
+++ app/actions.js
@@ -0,0 +1,19 @@
+/*
+steps needed for SQL fetch:
+create pool
+route
+    create the express route
+controller
+    create the controller (req, res)
+    hold response
+model
+    variable to hold the model
+    sql request
+    return result (the await pool)
+*/
+
+// "use server"
+
+// export async function fetch(){
+
+// }
