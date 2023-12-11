b5ecf2dd9e289f86a276c23394fe79bb38b60c2a
cal woodford <callum@woodford.info>
2023-12-11 09:57:45 +0000

    added: .env

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,9 @@
 export default function Contact() {
     return (
       <section className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>This is the Contact Page</p>
+        <p>This is the Contact Page!</p>
       </section>
     )
   }
+
+  
