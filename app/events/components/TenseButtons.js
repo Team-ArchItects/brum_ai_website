a755998c0828b605cf206c4725cd396452b23f19
Tay Khan <taykhantexas@gmail.com>
2023-12-12 16:45:27 +0200

    Styling Events Past Button

--- app/events/components/TenseButtons.js
+++ app/events/components/TenseButtons.js
@@ -1,8 +1,8 @@
 export default function TenseButtons ({futureOrPast}) {
     return (
         <div>
-            <button onClick={() => futureOrPast("future")} className="m-2 rounded-full p-2 border-4 border-citrus min-w-[150px]">Future</button>
-            <button onClick={() => futureOrPast("past")}>Past</button>
+            <button onClick={() => futureOrPast("future")} className="m-2 rounded-full p-2 border-4 border-citrus min-w-[150px] text-lg font-medium">Future</button>
+            <button onClick={() => futureOrPast("past")} className="m-2 rounded-full p-2 border-4 border-citrus min-w-[150px] text-lg font-medium">Past</button>
         </div>
     )
 }
