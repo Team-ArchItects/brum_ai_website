27ba13b8e54284992e22c9fdc7e6bf942c8b787a
Tay Khan <taykhantexas@gmail.com>
2023-12-12 16:35:14 +0200

    Future button styling events page

--- app/events/components/TenseButtons.js
+++ app/events/components/TenseButtons.js
@@ -1,7 +1,7 @@
 export default function TenseButtons ({futureOrPast}) {
     return (
         <div>
-            <button onClick={() => futureOrPast("future")}>Future</button>
+            <button onClick={() => futureOrPast("future")} className="m-2 rounded-full p-2 border-4 border-citrus min-w-[150px]">Future</button>
             <button onClick={() => futureOrPast("past")}>Past</button>
         </div>
     )
