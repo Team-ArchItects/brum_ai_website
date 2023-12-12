b5992976870bf1cf31c9f4c8883cb7e3bed664a1
David Mason <david_mason_@hotmail.co.uk>
2023-12-12 16:45:02 +0000

    Banner text changed to black

--- app/components/banner/Banner.js
+++ app/components/banner/Banner.js
@@ -12,11 +12,11 @@ const Banner = () => {
       <div
         className="row-start-1 mt-3 md:row-start-2 md:mt-0 min-w-[350px] justify-self-center col-start-6 lg:min-w-[450px] lg:col-start-2 lg:justify-self-start 2xl:col-start-2 2xl:justify-self-start bg-gradient-to-r from-magenta via-citrus to-aqua h-[270px] px-4 py-4 rounded-2xl">
         {/* <div className="flex justify-start space-x-4 p-2"> */}
-        <h1 className="font-bold text-3xl flex justify-start py-2 content-end ">Welcome to <Image src="./assets/BRUMAI_BLACK.svg" width="150" alt="brumAI logo" height="100" className="ml-2"/></h1>
+        <h1 className="font-bold text-3xl flex justify-start py-2 content-end text-black">Welcome to <Image src="./assets/BRUMAI_BLACK.svg" width="150" alt="brumAI logo" height="100" className="ml-2"/></h1>
         
            
         {/* </div>      */}
-        <p className="text-sm lg:text-base" >
+        <p className="text-sm text-black font-medium lg:text-base" >
           BrumAI is the home of AI in the Midlands. We are a dynamic community
           dedicated to fostering innovation, collaboration, and learning in the
           field of Artificial Intelligence. Our mission is to unite individuals
