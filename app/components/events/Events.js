d084c04bef54be0e5522953233433c3ec0c9746f
sam1234g <sam_german@hotmail.co.uk>
2023-12-13 13:34:58 +0000

    upcoming events images showing

--- app/components/events/Events.js
+++ app/components/events/Events.js
@@ -37,49 +37,69 @@ const Events = () => {
   export default Events;
   */
 
-'use client'
+  'use client'
 
-import supabase from '../../utils/supabase'
-import {useState, useEffect } from 'react';
-import NextEventsMap from './NextEventsMap';
+  import supabase from '../../utils/supabase'
+  import {useState, useEffect } from 'react';
+  import NextEventsMap from './NextEventsMap';
+  
+  const todayDate = new Date().toLocaleDateString();
+  
+  console.log(todayDate);
+  
+  //todayDate.replace('/','-')
+  
+  
+  console.log(todayDate);
+  
+  export default function EventSection() {
+      const [testEvents, setTestEvents] = useState(null);
+  
+      useEffect(() => {
+          const fetchEvents = async () => {
+            const { data, error } = await supabase
+              .from('testEvents')
+              .select('*')
+              .gt('display_until', todayDate)
+              .order('event_date', { ascending: true })
+              .limit(2);
+   console.log(data)     
+            if (error) {
+              console.error('Error:', error.message);
+              return;
+            }
+        
+            if (data) {
+              console.log('Data:', data); // Log the data variable to check the values
+        
+              // // Apply filtering and ordering
+              // const filteredData = data.filter(event => new Date(event.display_until) > new Date());
+              // const sortedData = filteredData.sort((a, b) => new Date(a.display_until) - new Date(b.display_until));
+              // const limitedData = sortedData.slice(0, 2);
+        
+               setTestEvents(data);
+            }
+          };
+        
+          fetchEvents();
+        }, []);
+  
+  return (
+    <section className="text-citrus text-center text-3xl py-2">
+            <h1 className="mt-8 text-6xl text-center p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">Upcoming Events</h1>
+                <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly">
 
-export default function EventSection() {
-    const [testEvents, setTestEvents] = useState(null);
 
-    useEffect(() => {
-        const fetchEvents = async () => {
-          const { data, error } = await supabase
-            .from('testEvents')
-            .select('*');
-      
-          if (error) {
-            console.error('Error:', error.message);
-            return;
-          }
-      
-          if (data) {
-            console.log('Data:', data); // Log the data variable to check the values
-      
-            // Apply filtering and ordering
-            const filteredData = data.filter(event => new Date(event.display_until) > new Date());
-            const sortedData = filteredData.sort((a, b) => new Date(a.display_until) - new Date(b.display_until));
-            const limitedData = sortedData.slice(0, 2);
-      
-            setTestEvents(limitedData);
-          }
-        };
-      
-        fetchEvents();
-      }, []);
+      {testEvents && testEvents.map?.((nextEvents) => (
+    <NextEventsMap key={nextEvents.id} data={nextEvents} />
 
-return (
-    <section>
-    {testEvents && testEvents.map?.((nextEvents) => (
-  <NextEventsMap key={nextEvents.id} data={nextEvents} />
-))}
-    </section>
-    // <div>
-    //     <p>{testEvents?.[1]?.event_description}</p>
-    // </div>
-)
-}
+   
+    
+  ))}
+      </section>
+      </section>
+      // <div>
+      //     <p>{testEvents?.[1]?.event_description}</p>
+      // </div>
+  )
+  }
--- app/components/events/NextEventsMap.js
+++ app/components/events/NextEventsMap.js
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 
+//
+
 export default function NextEventsMap({ data }) {
+  const date =new Date(data.event_date)
   return (
-    <a href={""}>
+    <a href={`${data.meetup_link}`}>
       <article className="border-2 border-citrus rounded-2xl my-5 p-5">
-        <h2 className="font-bold text-2xl">{`${data.event_name}`}</h2>
-        {/* <Image src={eventData.image_url} alt="Event photos" width="100" height="100" /> */}
+        <h1 className="font-bold text-2xl">{`${data.event_name}`}</h1>
+        <Image src={data.image_url} alt="Event photo" unoptimized width="500" height="300" className="justify-self-center align-self-center 2xl:min-w-[400px]" />
         <p>{`${data.event_description}`}</p>
         {/* <p>{`${""}`}</p> */}
+        <p>{`${date.toString().substring(4, 15)}`}</p>
+        <p>{`${data.event_time.substring(0,5)} - ${data.end_time.substring(0,5)}`}</p>
       </article>
     </a>
   );
