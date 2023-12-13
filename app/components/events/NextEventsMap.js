05c49e66268fadf9d58d7de5b55cb329940bc31c
sam1234g <sam_german@hotmail.co.uk>
2023-12-13 15:04:41 +0000

    future events background images displaying

--- app/components/events/NextEventsMap.js
+++ app/components/events/NextEventsMap.js
@@ -10,22 +10,22 @@ export default function NextEventsMap({ data }) {
   return (
     <a href={`${data.meetup_link}`}>
       <article
-      style={}
-        className={`border-2 border-citrus bg-[url(${bgImage})] bg-cover rounded-2xl my-5 p-5`}
+      style={{ backgroundImage: `url(${data.image_url})` }}
+        className={`border-2 border-citrus bg-cover bg-center rounded-2xl my-5 p-5`}
       >
-        <h1 className="font-bold text-2xl">{`${data.event_name}`}</h1>
-        <Image
+        <h1 className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] font-bold text-2xl">{`${data.event_name}`}</h1>
+        {/* <Image
           src={data.image_url}
           alt="Event photo"
           unoptimized
           width="500"
           height="300"
           className="justify-self-center align-self-center 2xl:min-w-[400px]"
-        />
-        <p>{`${data.event_description}`}</p>
+        /> */}
+        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">{`${data.event_description}`}</p>
         {/* <p>{`${""}`}</p> */}
-        <p>{`${date.toString().substring(4, 15)}`}</p>
-        <p>{`${data.event_time.substring(0, 5)} - ${data.end_time.substring(
+        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">{`${date.toString().substring(4, 15)}`}</p>
+        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">{`${data.event_time.substring(0, 5)} - ${data.end_time.substring(
           0,
           5
         )}`}</p>
