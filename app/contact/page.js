da72a15a1545be524ae065f203f6a507ec712da6
David Mason <david_mason_@hotmail.co.uk>
2023-12-13 11:22:19 +0000

    Dark/light theme toggle added throughout site. Small layout changes also made

--- app/contact/page.js
+++ app/contact/page.js
@@ -21,14 +21,14 @@ export const ContactUs = () => {
 
   return (
 
-    <div className=' flex flex-col justify-center items-center'>
-    <form ref={form} onSubmit={sendEmail} className='text-center mt-8 max-w-[600px]'>
+    <div className=' flex flex-col justify-center items-center '>
+    <form ref={form} onSubmit={sendEmail} className='text-center mt-8 w-10/12 max-w-[600px]'>
       <label className='newlabel'>Name</label>
-      <input className='border-0 outline-0 p-4 rounded-lg flex flex-col justify-center items-center w-full my-4 resize-none text-black min-w-[300px]' type="text" name="user_name" id='newInput'/>
+      <input className='border-0 outline-0 p-4 rounded-lg flex flex-col justify-center items-center w-full my-4 resize-none bg-zinc-700 text-white min-w-[300px]' type="text" name="user_name" id='newInput'/>
       <label className='newlabel mb-2'>Email</label>
-      <input className='border-0 outline-0 p-4 rounded-lg flex flex-col justify-center items-center w-full my-4 resize-none text-black min-w-[300px]' id='newInput' type="email" name="user_email" />
+      <input className='border-0 outline-0 p-4 rounded-lg flex flex-col justify-center items-center w-full my-4 resize-none bg-zinc-700 text-white min-w-[300px] ' id='newInput' type="email" name="user_email" />
       <label className='newlabel'>Message</label>
-      <textarea name="message" className='border-0 outline-0 p-4 rounded-lg flex flex-col justify-center items-center w-full mt-4 resize-none text-black min-w-[300px]' type="text" id='newInput'/>
+      <textarea name="message" className='border-0 outline-0 p-4 rounded-lg flex flex-col justify-center items-center w-full mt-4 resize-none text-black min-w-[300px] h-96 bg-zinc-700 text-white' type="text" id='newInput'/>
       <input type="submit" value="Send" id="input-submit" className='text-black bg-orange-500 cursor-pointer mt-8 px-6 py-2 rounded'/>
     </form>
     </div>
--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import YouTube from "./components/YouTube";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 ">
       <Banner />
       <EventSection />
       <Instagram />
