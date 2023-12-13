bc0f0bd4f0ced1ac362251e6b753d7b33e83dd12
KatBaginska <katarzyna.baginska11@icloud.com>
2023-12-13 12:21:57 +0000

    button future events

--- app/events/components/EventCard.js
+++ app/events/components/EventCard.js
@@ -1,15 +1,13 @@
+import Button from "@/app/components/banner/Button";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function EventCard({ eventData, timeFrame }) {
     const date =new Date(eventData.event_date)
   return (
-    <a
-      href={eventData.meetup_link}
-      className="w-9/12 flex flex-col items-center"
-    >
-      <article className="w-11/12 border-2 border-citrus rounded-2xl my-5 p-5">
-        <h2 className="font-bold text-2xl mb-5">{`${eventData.event_name}`}</h2>
+    
+      <article className="w-9/12 border-2 border-citrus rounded-2xl my-5 p-5 ">
+       <a href={eventData.meetup_link}><h2 className="font-bold text-2xl mb-5">{`${eventData.event_name}`}</h2></a>
         {/* {timeFrame === "past" && <Image src="https://images.unsplash.com/photo-1545350089-47b3d117a92c?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Event photo" width="150" height="150" />} */}
         <div className="grid grid-rows-2 2xl:grid-cols-[1fr_2fr] 2xl:grid-rows-none gap-6">
           <Image
@@ -26,9 +24,10 @@ export default function EventCard({ eventData, timeFrame }) {
             )}
             <p><span className="font-semibold underline">Venue</span>: {`${eventData.event_location}`}</p>
             <p><span className="font-semibold underline">Event details</span>: {`${eventData.event_description}`}</p>
+            {timeFrame === "future" &&(
+            <span className="self-end"><Button text={"Attend the Event"} location={eventData.meetup_link}></Button></span>)}
           </div>
         </div>
       </article>
-    </a>
   );
 }
