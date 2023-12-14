5740ff2e1f283f709dcc3a62253493b3764224d3
cal woodford <callum@woodford.info>
2023-12-14 16:51:54 +0000

    added: favicon | modified: widgets

--- app/components/EmbedTwitter.js
+++ app/components/EmbedTwitter.js
@@ -1,37 +1,16 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
 import Script from "next/script";
 
 export default function EmbedTwitter() {
-
-  const {resolvedTheme} = useTheme()
-  let src;
-  let dataId;
-  switch(resolvedTheme) {
-    case 'light':
-      src = 'https://widgets.sociablekit.com/twitter-feed/widget.js';
-      dataId = "242041";
-      break;
-    case 'dark':
-      src = 'https://widgets.sociablekit.com/twitter-feed/widget.js';
-      dataId = "241650";
-      break;
-    default:       
-    src = 'https://widgets.sociablekit.com/twitter-feed/widget.js';
-    dataId = "241650";
-    break;
-
-  }
   return (
     <>
-      <div className="sk-ww-twitter-feed" data-embed-id={dataId}></div>
+      <div class="sk-ww-twitter-feed" data-embed-id="241650"></div>
       <Script
-        src={src}
+        src="https://widgets.sociablekit.com/twitter-feed/widget.js"
         async
         defer
       ></Script>
-      <br></br>
     </>
   );
 }
