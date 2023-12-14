90c3356ebef9d379054852507cc681a635fe0831
cal woodford <callum@woodford.info>
2023-12-14 11:25:05 +0000

    added: event button images

--- app/components/events/NextEventsMap.js
+++ app/components/events/NextEventsMap.js
@@ -5,15 +5,16 @@ import Link from "next/link";
 
 export default function NextEventsMap({ data }) {
   const date = new Date(data.event_date);
-  const bgImage =
-    "https://images.unsplash.com/photo-1545350089-47b3d117a92c?q=80&w=3866&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
   return (
-    <a href={`${data.meetup_link}`}>
+
       <article
-      style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url(${data.image_url})` } }
-        className={`border-2 border-citrus bg-cover bg-center rounded-2xl my-5 p-5`}
+      style={{ backgroundImage: `linear-gradient(rgba(255, 130, 46, 0.6), rgba(255, 130, 46, 0.6)), url(${data.image_url})` } }
+        className={`border-4 border-citrus bg-cover bg-center rounded-2xl my-5 p-5 w-[470px]`} // give a width and height
       >
-        <h1 className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] font-bold text-2xl">{`${data.event_name}`}</h1>
+        <a href={`${data.meetup_link}`}>
+        <h1 className="text-black font-bold text-2xl">{`${data.event_name}`}</h1>
+        {/* // drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)] */}
         {/* <Image
           src={data.image_url}
           alt="Event photo"
@@ -22,14 +23,14 @@ export default function NextEventsMap({ data }) {
           height="300"
           className="justify-self-center align-self-center 2xl:min-w-[400px]"
         /> */}
-        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">{`${data.event_description}`}</p>
+        <p className="text-black font-bold">{`${data.event_description}`}</p>
         {/* <p>{`${""}`}</p> */}
-        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">{`${date.toString().substring(4, 15)}`}</p>
-        <p className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">{`${data.event_time.substring(0, 5)} - ${data.end_time.substring(
+        <p className="text-black font-bold">{`${date.toString().substring(4, 15)}`}</p>
+        <p className="text-black font-bold">{`${data.event_time.substring(0, 5)} - ${data.end_time.substring(
           0,
           5
         )}`}</p>
+        </a>
       </article>
-    </a>
   );
 }
