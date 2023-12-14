0a487f3a21124719786821d853b208d058af20f0
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-14 12:35:17 +0000

    sponsors updated

--- app/data/data.js
+++ app/data/data.js
@@ -1,89 +1,113 @@
 export const pages = [
-    {
-        id: 1,
-        label: "Homepage",
-        link: "/"
-    },
-    {
-        id: 2,
-        label: "Events",
-        link: "/events"
-    },
-    {
-        id: 3,
-        label: "Contact",
-        link: "/contact"
-    }
-]
+  {
+    id: 1,
+    label: "Homepage",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "Events",
+    link: "/events",
+  },
+  {
+    id: 3,
+    label: "Contact",
+    link: "/contact",
+  },
+];
 
 export const headerSocials = [
-    {
-        id: 1,
-        name: "Meetup",
-        link: "https://www.meetup.com/brum-ai/",
-        imageDark: "/assets/meetup_white.svg",
-        alt: "Meetup logo linking to the BRUM AI Meetup page"
-    },
-    {
-        id: 2,
-        name: "LinkedIn",
-        link: "https://www.linkedin.com/company/brumai/",
-        imageDark: "/assets/linkedin_icon_white.svg",
-        alt: "LinkedIn logo linking to the BRUM AI LinkedIn page"
-    },
-    {
-        id: 3,
-        name: "Instagram",
-        link: "https://www.instagram.com/brum_ai_/",
-        imageDark: "/assets/instagram_icon_white.svg",
-        alt: "Instagram logo linking to the BRUM AI instagram page"
-    },
-    {
-        id: 4,
-        name: "Youtube",
-        link: "https://www.youtube.com/@brumai5129",
-        imageDark: "/assets/youtube_icon_white.svg",
-        alt: "Youtube logo linking to the BRUM AI Youtube page"
-    },
-    {
-        id: 5,
-        name: "Discord",
-        link: "https://discord.gg/RJSU67kZcJ",
-        imageDark: "/assets/discord_white.svg",
-        alt: "Discord logo linking to the BRUM AI discord group"  
-    },
-    {
-        id: 6,
-        name: "Twitter",
-        link: "https://x.com/brum_AI",
-        imageDark: "/assets/x_white.png",
-        alt: "X logo linking to the BRUM AI X page"
-    },
-]
+  {
+    id: 1,
+    name: "Meetup",
+    link: "https://www.meetup.com/brum-ai/",
+    imageDark: "/assets/meetup_white.svg",
+    alt: "Meetup logo linking to the BRUM AI Meetup page",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/company/brumai/",
+    imageDark: "/assets/linkedin_icon_white.svg",
+    alt: "LinkedIn logo linking to the BRUM AI LinkedIn page",
+  },
+  {
+    id: 3,
+    name: "Instagram",
+    link: "https://www.instagram.com/brum_ai_/",
+    imageDark: "/assets/instagram_icon_white.svg",
+    alt: "Instagram logo linking to the BRUM AI instagram page",
+  },
+  {
+    id: 4,
+    name: "Youtube",
+    link: "https://www.youtube.com/@brumai5129",
+    imageDark: "/assets/youtube_icon_white.svg",
+    alt: "Youtube logo linking to the BRUM AI Youtube page",
+  },
+  {
+    id: 5,
+    name: "Discord",
+    link: "https://discord.gg/RJSU67kZcJ",
+    imageDark: "/assets/discord_white.svg",
+    alt: "Discord logo linking to the BRUM AI discord group",
+  },
+  {
+    id: 6,
+    name: "Twitter",
+    link: "https://x.com/brum_AI",
+    imageDark: "/assets/x_white.png",
+    alt: "X logo linking to the BRUM AI X page",
+  },
+];
 
 export const footerSponsors = [
-    {
-        id: 1,
-        name: "School of Code",
-        imageLight: "/assets/original.png",
-        imageDark: "/assets/original.png",
-        alt: "School of Code logo",
-        url: "",
-    },
-    {
-        id: 2,
-        name: "University of Aston",
-        imageLight: "/assets/aston_white.png",
-        imageDark: "/assets/aston_black.png",
-        alt: "University of Aston logo",
-        url: "",
-    },
-    {
-        id: 3,
-        name: "Huel",
-        imageLight: "/assets/huel_white.png",
-        imageDark: "/assets/huel_black.png",
-        alt: "Huel Logo",
-        url: "",
-    }
-]
+  {
+    id: 1,
+    name: "University of Aston",
+    imageLight: "/assets/aston_white.png",
+    imageDark: "/assets/aston_black.png",
+    alt: "University of Aston logo",
+    url: "https://www.aston.ac.uk/research/eps/acaira",
+  },
+  {
+    id: 2,
+    name: "DRPG",
+    imageLight: "/assets/DRPG_logo_MASTER_WHITE.png",
+    imageDark: "/assets/DRPG_logo_MASTER_BLACK.png",
+    alt: "DRPG logo",
+    url: "https://www.drpgroup.com/",
+  },
+  {
+    id: 3,
+    name: "Huel",
+    imageLight: "/assets/huel_logo_white.png",
+    imageDark: "/assets/huel_logo_black.png",
+    alt: "Huel Logo",
+    url: "https://careers.huel.com/",
+  },
+  {
+    id: 4,
+    name: "WealthWizards",
+    imageLight: "/assets/wealth_logo_white.png",
+    imageDark: "/assets/wealth_logo_black.png",
+    alt: "WealthWizards logo",
+    url: "https://www.wealthwizards.com/",
+  },
+  {
+    id: 5,
+    name: "School of Code",
+    imageLight: "/assets/soc_logo.png",
+    imageDark: "/assets/soc_logo.png",
+    alt: "School of Code logo",
+    url: "https://www.schoolofcode.co.uk/",
+  },
+  {
+    id: 6,
+    name: "ML Developer Programmes at Google",
+    imageLight: "/assets/google_dev_logo.png",
+    imageDark: "/assets/google_dev_logo.png",
+    alt: "ML Developer Programmes at Google logo",
+    url: "https://medium.com/google-developer-experts",
+  },
+];
