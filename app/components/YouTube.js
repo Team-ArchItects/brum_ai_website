e4ed27ab884f8ca7e05bcaf8035ae656596c7f28
cal woodford <callum@woodford.info>
2023-12-14 13:59:00 +0000

    modified: event button style, banner image, margins

--- app/components/YouTube.js
+++ app/components/YouTube.js
@@ -4,7 +4,7 @@ export default function YouTube() {
   return (
 
     <>
-<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly mt-10 mb-10">
+<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 place-content-evenly mb-10">
     <CardMedia
     component="iframe"
     width="560" height="315"
