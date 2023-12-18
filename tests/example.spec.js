9c1ba6f15e35447dc28e1a49604d0c47536eb7e4
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-18 15:37:20 +0000

    test added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,3 +16,23 @@ test("more events button to events page", async ({ page }) => {
 
   await expect(page).toHaveTitle("Brum.AI | Events");
 });
+
+//simple user jounrey
+test("user loads the page, goes to events page, goes to past events, loads five more, clicks an event", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000");
+
+  await page.getByRole("link", { name: "More Events Arrow" }).click();
+
+  await page.getByRole("button", { name: "Past Events" }).click();
+
+  await page.getByRole("button", { name: "Show More" }).click();
+
+  await page.getByRole("button", { name: "Future Events" }).click();
+
+  // await page
+  // .getByRole("button", { name: "Attend the Event Arrow" }).click();
+
+  await expect(page).toHaveTitle("Brum.AI | Events");
+});
