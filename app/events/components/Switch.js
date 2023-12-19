f638afbadef462e44f4901084a8c04876add6397
David Mason <david_mason_@hotmail.co.uk>
2023-12-19 09:50:59 +0000

    Calendar styling started

--- app/events/components/Switch.js
+++ app/events/components/Switch.js
@@ -1,19 +1,34 @@
-"use client"
-import { Switch } from 'antd';
-import { useState } from 'react';
+"use client";
+import { Switch, ConfigProvider } from "antd";
+import { useState } from "react";
 
-const CalendarSwitcher = () => { 
-    const [switchChange, setSwitchChange] = useState(true);
-    const onChange = (checked) => {
-        setSwitchChange(prevSwitch => !prevSwitch);
-        console.log(`switch to ${checked}`);
-        console.log(switchChange)
-      };
+const CalendarSwitcher = () => {
+  const [switchChange, setSwitchChange] = useState(true);
+  const onChange = (checked) => {
+    setSwitchChange((prevSwitch) => !prevSwitch);
+    console.log(`switch to ${checked}`);
+    console.log(switchChange);
+  };
 
-    return (
+  return (
+    <ConfigProvider 
+      theme={{
+        components: {
+          Switch: {
+            colorPrimary: '#E708FF',
+            colorPrimaryBorder: '#FF822E',
+            colorPrimaryHover: '#BF047D',
+            handleBg: '#FF822E',
+            colorPrimaryBg: '#FF822E',
+            colorTextTertiary: '#FF822E'
 
-<Switch onChange={onChange} handleBg={"#fff"} handleShadow={"#fff"}
-
-/>)}
+          }
+        }
+      }}
+    >
+      <Switch onChange={onChange} className=""/>
+    </ConfigProvider>
+  );
+};
 
 export default CalendarSwitcher;
