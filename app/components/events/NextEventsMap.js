05274db77f0c1de5043bcd376c18e8f3f5161819
Tay Khan <taykhantexas@gmail.com>
2023-12-20 14:29:26 +0200

    Blur added to Event Image

--- app/components/events/NextEventsMap.js
+++ app/components/events/NextEventsMap.js
@@ -5,11 +5,12 @@ const image = data.image_url || "assets/placeholder_event_image.jpg";
     <a href={`${data.meetup_link}`} target="_blank" rel="noopener noreferrer"className="w-fit">
       <article
         style={{
-          backgroundImage: `linear-gradient(rgba(255, 130, 46, 0.6), rgba(255, 130, 46, 0.6)), url(${image})`,
+          backgroundImage: `linear-gradient(rgba(255, 130, 46, 0.8), rgba(255, 130, 46, 0.8)), url(${image})`,
         }}
         className={`border-4 border-citrus bg-cover bg-center rounded-2xl mt-5 mb-3 p-5 w-full max-w-[600px] justify-self-center`}
       >
-        <h1 className="text-black font-bold text-3xl ">{`${data.event_name}`}</h1>
+        <div className="backdrop-blur-sm">
+        <h1 className="text-black font-bold text-3xl backdrop-blur-sm">{`${data.event_name}`}</h1>
 
         <p className="text-black font-bold">{`${date
           .toString()
@@ -18,6 +19,8 @@ const image = data.image_url || "assets/placeholder_event_image.jpg";
           0,
           5
         )} - ${data.end_time.substring(0, 5)}`}</p>
+        </div>
+
       </article>
     </a>
   );
