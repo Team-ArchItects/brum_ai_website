a9172d6297d55a7e981bbc5bfb5593f7261dc180
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-21 10:30:43 +0000

    about names aligned

--- app/aboutus/Aboutus.js
+++ app/aboutus/Aboutus.js
@@ -3,8 +3,8 @@ import LinkedInImage from "./LinkedInImage";
 
 export default function AboutUsMap({ data }) {
   return (
-    <section className="grid grid-cols-3">
-      <div className="col-span-1 flex justify-center items-center p-4">
+    <section className="grid grid-cols-3 ">
+      <div className="col-span-1 flex items-center p-6">
         <Image
           src={data.imageUrl}
           alt={`Profile photo of ${data.name}`}
@@ -13,7 +13,7 @@ export default function AboutUsMap({ data }) {
           className="rounded-full aspect-[100/99]"
         />
       </div>
-      <div className="col-span-2 p-4">
+      <div className="col-span-2 p-6">
         <h3 className="text-citrus">{data.name}</h3>
         <p className="italic">{data.role}</p>
         <p className="">{data.description}</p>
--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -22,21 +22,21 @@ export default function AboutUs() {
           <p>{aboutUsCopy.aboutUsP3}</p>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-between ">
-        <h1 className="mt-8 text-2xl md:text-3xl text-center p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">
+      <div className="flex flex-col items-center">
+        <h1 className="mt-8 text-2xl md:text-3xl p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">
           Brum AI Team
         </h1>
-        <div className="items-center grid grid-cols-1 md:grid-cols-2 gap-x-4 w-full lg:w-9/12 space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 w-full lg:w-9/12">
           {coreTeamData.map((coreTeam) => {
             return <AboutUsMap key={coreTeam.id} data={coreTeam} />;
           })}
         </div>
       </div>
-      <div className="flex flex-col items-center justify-between ">
-        <h1 className="mt-8 text-2xl md:text-3xl text-center p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">
+      <div className="flex flex-col items-center">
+        <h1 className="mt-8 text-2xl md:text-3xl p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">
           Development Team
         </h1>
-        <div className="items-center grid grid-cols-1 md:grid-cols-3 gap-x-4 w-full lg:w-9/12 space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4 w-full lg:w-9/12 ">
           {websiteTeamData.map((websiteTeam) => {
             return <AboutUsMap key={websiteTeam.id} data={websiteTeam} />;
           })}
