4c11ac6d204092720dddce8b782bdc0ab610540e
Alexander <142804583+alexjbrowntech@users.noreply.github.com>
2023-12-21 11:01:52 +0000

    images about us top

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function AboutUs() {
   return (
     <section>
-      <div className="flex flex-col items-center justify-between ">
+      <div className="flex flex-col items-center justify-between">
         <h1 className="mt-8 text-2xl md:text-3xl text-center p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">
           {aboutUsCopy.title}
         </h1>
@@ -32,11 +32,11 @@ export default function AboutUs() {
           })}
         </div>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center pb-8">
         <h1 className="mt-8 text-2xl md:text-3xl p-4 bg-clip-text text-transparent bg-gradient-to-r from-magenta via-citrus to-aqua">
           Development Team
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4 w-full lg:w-9/12 ">
+        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-x-4 w-full lg:w-9/12">
           {websiteTeamData.map((websiteTeam) => {
             return <AboutUsMap key={websiteTeam.id} data={websiteTeam} />;
           })}
