7b0d83873a98c40612b2f35e629542aab3192fc3
calwoodford <callum@woodford.info>
2024-01-25 17:17:15 +0000

    modified: bull banner image png > jpg

--- app/components/banner/Banner.js
+++ app/components/banner/Banner.js
@@ -5,7 +5,7 @@ import { homepageData } from "@/app/data/data";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('/assets/bull_photo_banner.png')] bg-cover bg-center w-screen h-96 grid grid-cols-11 md:grid-cols-12 grid-rows-6">
+    <div className="bg-[url('/assets/bull_photo_banner.jpg')] bg-cover bg-center w-screen h-96 grid grid-cols-11 md:grid-cols-12 grid-rows-6">
       <section className="flex col-start-11 row-start-6 justify-self-end md:justify-self-end md:row-start-2 mr-3">
         <Button text={"Next event"} location={"/events"} />
       </section>
